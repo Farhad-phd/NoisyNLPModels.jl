99ac07e104045001c2565db2dce06762c9b4d254
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2024-02-15 17:45:49 -0500

    unit tests

--- src/NoisyNLPModels.jl
+++ src/NoisyNLPModels.jl
@@ -1,10 +1,10 @@
 module NoisyNLPModels
 
 export RandomNoisyNLPModel
-
-import NLPModels.obj
 using NLPModels
+import NLPModels.obj
 import NLPModels.grad!
+import NLPModels.grad
 
 # See https://jso.dev/NLPModels.jl/stable/guidelines/#bare-minimum
 abstract type AbstractNoisyNLPModel{T, S} <: AbstractNLPModel{T, S} end
@@ -28,17 +28,21 @@ RandomNoisyNLPModel(model::AbstractNLPModel{T, S}, noise_level) where {T, S} =
   RandomNoisyNLPModel(model, convert(T, noise_level))
 
 # noisy objective
-function obj(model::RandomNoisyNLPModel{T, S, M}, x::S) where {T, S, M}
+function obj(model::RandomNoisyNLPModel{T, S, M}, x::S) where {T, S <: (AbstractVector), M}
   return obj(model.base_model, x) + model.noise_level * (rand(T) - 1 / 2)
 end
 
 # noisy objective with given noise by user (not the one stored in the model)
-function obj(model::RandomNoisyNLPModel{T, S, M}, x::S, noise::T) where {T, S, M}
+function obj(
+  model::RandomNoisyNLPModel{T, S, M},
+  x::S,
+  noise::T,
+) where {T, S <: (AbstractVector), M}
   return obj(model.base_model, x) + noise
 end
 
 # noisy gradient
-function grad!(model::RandomNoisyNLPModel{T, S, M}, x::S, g::S) where {T, S, M}
+function grad!(model::RandomNoisyNLPModel{T, S, M}, x::S, g::S) where {T, S <: (AbstractVector), M}
   grad!(model.base_model, x, g)
   for i ∈ eachindex(g)
     g[i] += model.noise_level * (rand(T) - 1 / 2)
@@ -47,11 +51,37 @@ function grad!(model::RandomNoisyNLPModel{T, S, M}, x::S, g::S) where {T, S, M}
 end
 
 # noisy gradient with given noise by user (not the one stored in the model)
-function grad!(model::RandomNoisyNLPModel{T, S, M}, x::S, g::S, noise::T) where {T, S, M}
+function grad!(
+  model::RandomNoisyNLPModel{T, S, M},
+  x::S,
+  g::S,
+  noise::T,
+) where {T, S <: (AbstractVector), M}
   grad!(model.base_model, x, g)
   for i ∈ eachindex(g)
     g[i] += noise
   end
   g
 end
+
+function grad(model::RandomNoisyNLPModel{T, S, M}, x::S) where {T, S <: (AbstractVector), M}
+  g = grad(model.base_model, x)
+  for i ∈ eachindex(g)
+    g[i] += model.noise_level * (rand(T) - 1 / 2)
+  end
+  g
+end
+
+function grad(
+  model::RandomNoisyNLPModel{T, S, M},
+  x::S,
+  noise::T,
+) where {T, S <: (AbstractVector), M}
+  g = grad(model.base_model, x)
+  for i ∈ eachindex(g)
+    g[i] += noise
+  end
+  g
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,47 +6,59 @@ using ADNLPModels
 @testset "NoisyNLPModels Tests" begin
   # Create a base model for testing
   base_model = ADNLPModel(x -> (x[1] - 1.0)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
+  model = RandomNoisyNLPModel(base_model, 0.1)
+  x = model.meta.x0
+  g_base = grad(base_model, x)
+  obj_base = obj(base_model, x)
+  T = eltype(x)
+  noise = T(0.2)
 
   # Test the constructor
   @testset "RandomNoisyNLPModel constructor" begin
-    model = RandomNoisyNLPModel(base_model, 0.1)
     @test model.base_model == base_model
     @test model.noise_level == 0.1
   end
 
   # Test the obj function
   @testset "obj function" begin
-    model = RandomNoisyNLPModel(base_model, 0.1)
-    x = model.meta.x0
-    @test abs(obj(model, x) - obj(base_model, x)) <= model.noise_level * 1 / 2
-
+    @test abs(obj(model, x) - obj_base) <= model.noise_level * 1 / 2
     # Test the obj function with a given noise
-    noise = 0.2
-    @test abs(obj(model, x, noise) - obj(base_model, x) - noise) <= 1e-10
+
+    @test abs(obj(model, x, noise) - obj_base - noise) <= 1e-10
   end
 
   # Test the grad! function
   @testset "grad! function" begin
-    model = RandomNoisyNLPModel(base_model, 0.1)
-    x = model.meta.x0
     g = similar(x)
     #testing the gradient
     grad!(model, x, g)
-    @test g .- grad(base_model, x) .< model.noise_level * (1 / 2)
+
+    for i ∈ eachindex(g)
+      @test abs(g[i] - g_base[i]) <= model.noise_level * 1 / 2
+    end
 
     # Test the grad! function with a given noise
-    noise = 0.2
+
     g = similar(x)
     grad!(model, x, g, noise)
-    @test g == grad(base_model, x) .+ noise
+
+    for i ∈ eachindex(g)
+      @test g[i] == g_base[i] .+ noise
+    end
 
     # Test the grad function 
     g = grad(model, x)
-    @test g .- grad(base_model, x) .< model.noise_level * (1 / 2)
+
+    for i ∈ eachindex(g)
+      @test abs(g[i] - g_base[i]) <= model.noise_level * 1 / 2
+    end
 
     # Test the grad function with a given noise
-    noise = 0.2
+
     g = grad(model, x, noise)
-    @test g == grad(base_model, x) .+ noise
+
+    for i ∈ eachindex(g)
+      @test g[i] == g_base[i] .+ noise
+    end
   end
 end
